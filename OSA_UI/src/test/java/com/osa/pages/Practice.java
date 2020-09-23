15f505ca9afe93b2f7f826a8fd781b2c34afe009
RifatBJ <59144992+RifatBJ@users.noreply.github.com>
2020-09-23 12:53:04 -0400

    Adding practice class

---
+++ OSA_UI/src/test/java/com/osa/pages/Practice.java
@@ -0,0 +1,8 @@
+package com.osa.pages;
+
+public class Practice {
+	public void name() {
+		System.out.println("This is Rifat");
+	}
+
+}
--- OSA_UI/src/test/java/com/osa/pages/Tom.java
+++
@@ -1,8 +0,0 @@
-package com.osa.pages;
-
-public class Tom {
-	public void num() {
-		System.out.println("This is rifat");
-	}
-
-}
